7323b8788a7e27b83d0b60a2195db63013b6cb01
Vadim Volodin <sironlyou@gmail.com>
2023-03-30 15:33:59 +0300

    another fix

--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -9,7 +9,7 @@ const CLIENT_ID = "TtTT3iNgIdJIJ1X2EwAlBA";
 export function UserBlock({ avatarSrc, username, loading }: IUserBlockProps) {
   return (
     <a
-      href={`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity`}
+      href={`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=code&state=random_string&redirect_uri=https://reddit-mirror.onrender.com/authauth&duration=permanent&scope=read submit identity`}
       className={styles.userBox}>
       <div className={styles.avatarBox}>{avatarSrc ? <img src={avatarSrc} alt="user avatar" className={styles.avatarImage} /> : <IconAnon />}</div>
       <div className={styles.username}>

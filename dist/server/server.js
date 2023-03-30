2be591d82aaea692813cf5bf4e01b13550019bbc
Vadim Volodin <sironlyou@gmail.com>
2023-03-30 15:22:11 +0300

    redirect

--- dist/server/server.js
+++ dist/server/server.js
@@ -393,7 +393,7 @@ module.exports = require("react-dom/server");
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.indexTemplate = void 0;
-var indexTemplate = function (content, token) { return "\n<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n  <meta charset=\"UTF-8\">\n  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <title>Reddit</title>\n  <script src=\"/static/client.js\" type=\"application/javascript\"></script>\n</head>\n\n<body>\n  <div id=\"react_root\">".concat(content, "</div>\n  <div id=\"modal_root\"></div> \n  <div id=\"dropdown_root\"></div>\n</body>\n\n</html>\n"); };
+var indexTemplate = function (content) { return "\n<!DOCTYPE html>\n<html lang=\"en\">\n\n<head>\n  <meta charset=\"UTF-8\">\n  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n  <title>Reddit</title>\n  <script src=\"/static/client.js\" type=\"application/javascript\"></script>\n</head>\n\n<body>\n  <div id=\"react_root\">".concat(content, "</div>\n  <div id=\"modal_root\"></div> \n  <div id=\"dropdown_root\"></div>\n</body>\n\n</html>\n"); };
 exports.indexTemplate = indexTemplate;
 
 
